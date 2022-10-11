5937060eef2e331aec292479a044f7c3346967dd
Yuki1005 <yhiramatsu5@icloud.com>
2022-10-11 16:09:56 +0900

    aaaa

--- m_m_2_queue.py
+++ m_m_2_queue.py
@@ -1,6 +1,14 @@
+import csv
 import pandas as pd
+import datetime as dt
+
 end_time = [0,0]
-pattern1 = pd.read_csv("path.csv",usecols=["Time","Go","Gate"])
-a1 = pattern1[((pattern1["Time"]))]
+
+pattern1 = pd.read_csv("path.csv")
+with open("path.csv",encoding="utf_8") as f:
+    reader = csv.reader(f)
+    line=[row for row in reader]
+    
 for i in range(len(pattern1)):
-    print(a1[1])
+    aa1 = line[i+1][0].replace(":"," ").split()
+    
