c98f77fcd506043d6ae2995db5182001d79b8326
Yuki1005 <yhiramatsu5@icloud.com>
2022-10-11 15:32:12 +0900

    a

--- m_m_2_queue.py
+++ m_m_2_queue.py
@@ -1,5 +1,5 @@
 import pandas as pd
 end_time = [0,0]
-pattern1 = pd.read_csv("path.csv",)
+pattern1 = pd.read_csv("path.csv",usecols=["Time","Go","Gate"])
 for i in range(len(pattern1)):
-    
+    print(pattern1[1])
