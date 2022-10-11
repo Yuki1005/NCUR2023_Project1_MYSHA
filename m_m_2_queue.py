1f71827d9bab7cc282dd4b7af2dbe12c3cae8514
Yuki1005 <yhiramatsu5@icloud.com>
2022-10-11 14:45:09 +0900

    mastar

--- m_m_2_queue.py
+++ m_m_2_queue.py
@@ -1,4 +1,1 @@
-import pandas as pd
-end_time = [0,0]
-pattern1 = pd.read_csv("a.csv")
-print(pattern1.shape)
+counter = 2
--- new_project.py
+++ new_project.py
@@ -108,12 +108,13 @@ for i in G:
     elif i == nx.dijkstra_path(G,aaa,bbb)[0]:
         color_map.append('crimson')
     else: color_map.append('skyblue')   
-## スタートからの全ノード間距離データ
-#print(nx.single_source_dijkstra_path_length(G, aaa))
 
-
+print(nx.single_source_dijkstra_path_length(G, aaa))
 nx.draw_networkx(G,pos,edge_color=edge_color_list,node_color= color_map)     #座標あり
 #nx.draw_networkx(G)        #座標なし
 plt.show()
 
-#設定　ワークスペース　post commit command push
+# 平均kts(knot)を測る。1kts = 0.51477m/s
+# 距離データをもう一度見直し
+# 待ち行列プログラム
+# #1をもとに各ゲートから離陸地点までの平均到達時間を予想、
