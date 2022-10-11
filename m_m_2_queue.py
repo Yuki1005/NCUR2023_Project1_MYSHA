b7d77e4674281f1577f70ed389c586f581652c22
Yuki1005 <yhiramatsu5@icloud.com>
2022-10-11 15:16:49 +0900

    a

--- m_m_2_queue.py
+++ m_m_2_queue.py
@@ -1,4 +1,5 @@
 import pandas as pd
 end_time = [0,0]
-pattern1 = pd.read_csv("a.csv")
-print(len(pattern1.columns))
+pattern1 = pd.read_csv("a.csv",)
+for i in range(len(pattern1)):
+    print(pattern1)
