945ca394dc495a11f730e99882fe142656a38537
Yuki1005 <yhiramatsu5@icloud.com>
2022-12-22 09:09:15 +0900

    aaa

--- 最終コード.py
+++ 最終コード.py
@@ -16,7 +16,7 @@ wait_time_arrive = 0
 
 kankaku = 60
 
-G = nx.read_weighted_edgelist("dijkstra_data2.txt", create_using=nx.DiGraph)
+G = nx.read_weighted_edgelist("福岡空港距離データ.txt", create_using=nx.DiGraph)
 pattern1 = pd.read_csv("離着陸データ.csv")
 
 def nakami():
